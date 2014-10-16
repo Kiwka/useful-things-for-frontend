df007e417765d934164357d519d66a797f06b51d
Olena Sovyn <olenasovyn@gmail.com>
2014-10-16 14:01:55 +0300

    Gulpfile added
    Gulpfile from the gulp training in the front-end school (Cogniance, Kyiv)

---
+++ Gulpfile.js
@@ -0,0 +1,39 @@
+{
+  "name": "gulp-presentation",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "connect": "^3.2.0",
+    "connect-livereload": "^0.4.0",
+    "grunt": "^0.4.5",
+    "grunt-contrib-uglify": "^0.6.0",
+    "gulp": "^3.8.8",
+    "gulp-concat": "^2.4.1",
+    "gulp-csso": "^0.2.9",
+    "gulp-imagemin": "^1.0.1",
+    "gulp-jade": "^0.8.0",
+    "gulp-livereload": "^2.1.1",
+    "gulp-minify-css": "^0.3.10",
+    "gulp-myth": "^1.0.1",
+    "gulp-stylus": "^1.3.3",
+    "gulp-uglify": "^1.0.1",
+    "gulp-util": "^3.0.1",
+    "serve-static": "^1.6.3",
+    "tiny-lr": "^0.1.4"
+  },
+  "devDependencies": {
+    "grunt-contrib-concat": "^0.5.0",
+    "gulp-autoprefixer": "^1.0.1",
+    "gulp-connect": "^2.0.6",
+    "gulp-csslint": "^0.1.5",
+    "gulp-imagemin": "^1.0.1",
+    "gulp-plumber": "^0.6.6",
+    "gulp-webserver": "^0.8.3"
+  }
+}
